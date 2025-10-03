d9cfa29e08afda24b9530cf8cc963a7e5c87a685
Tai Nguyen Van <buiblue123@gmail.com>
2025-10-03 23:55:52 +0700

    style: change font -> roboto

--- tailwind.config.js
+++ tailwind.config.js
@@ -42,7 +42,7 @@ export default {
         },
       },
       fontFamily: {
-        sans: ["Inter", "system-ui", "sans-serif"],
+        sans: ["Roboto", "system-ui", "sans-serif"],
         display: ["Poppins", "system-ui", "sans-serif"],
       },
       animation: {
